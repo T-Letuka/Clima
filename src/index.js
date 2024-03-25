183436afee87a6214e70cef86748f6bdd2857911
T-Letuka <tisetsoletuka@gmail.com>
2024-03-25 10:51:43 +0200

    added search option connected to api

--- src/index.js
+++ src/index.js
@@ -23,3 +23,31 @@ let actaulTime = `${hour}:${minutes}`;
 
 let currentDay = document.getElementById("actual-day");
 currentDay.innerHTML = `${actualDay} ${actaulTime}`;
+
+//SEARCH FORM AND TEMPERATURE
+let form = document.getElementById("search-form");
+function submitButton(event) {
+  event.preventDefault();
+
+  let cityInput = document.getElementById("input-search").value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(currentWeather);
+}
+
+function currentWeather(response) {
+  console.log(response.data);
+  let newCity = document.querySelector("#new-city");
+  let newTemperature = document.querySelector("#new-temperature");
+  let city = response.data.city;
+  let temperature = Math.round(response.data.temperature.current);
+
+  newCity.innerHTML = city;
+  newTemperature.innerHTML = `${temperature}`;
+}
+form.addEventListener("submit", function (event) {
+  submitButton(event);
+});
+
+let apiKey = "06e466583a141dtb4eba022fc8fboc4e";
+let city = "Paris";
