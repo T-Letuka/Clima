e914f39507a3e6eb588e04fd87f4c81a43f4aacd
T-Letuka <tisetsoletuka@gmail.com>
2024-04-03 12:39:10 +0200

    added icons and restyled the forecast

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const actualDay = days[actualDayIndex];
 let hour = currentDate.getHours();
 let minutes = currentDate.getMinutes();
 
-// Add leading zeros to single-digit hours and minutes
+// Adding leading zeros to single-digit hours and minutes
 hour = hour < 10 ? "0" + hour : hour;
 minutes = minutes < 10 ? "0" + minutes : minutes;
 
@@ -31,13 +31,16 @@ currentDay.innerHTML = `${actualDay} ${actualTime}`;
 // Search form and temperature
 const form = document.getElementById("search-form");
 
+function findCity(city) {
+  let apiKey = "06e466583a141dtb4eba022fc8fboc4e";
+  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
 function submitButton(event) {
   event.preventDefault();
-
-  const cityInput = document.getElementById("input-search").value;
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(currentWeather);
+  let cityInput = document.getElementById("input-search");
+  findCity(cityInput.value);
 }
 
 function currentWeather(response) {
@@ -57,27 +60,50 @@ function currentWeather(response) {
   descriptionElement.innerHTML = `,${description}`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
-
-  console.log(response.data);
+  getForecast(response.data.city);
 }
 
-function displayForecast() {
-  let forecast = document.querySelector("#forecast");
-  let days = ["Mon", "Tue", "Wed", "Thur", "Fri"];
+function getForecast(city) {
+  let apiKey = "06e466583a141dtb4eba022fc8fboc4e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  days.forEach(function (day) {
-    forecast.innerHTML += `
-      <div class="forecast-day">
-        <div class="date-forecast">${day}</div>
-        <div class="weather-icon">⛈</div>
-        <div class="forecast-temperature">
-          <span class="temperature-deg">12</span> 9
+  return days[date.getDay()];
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5)
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div >
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        </div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong class="Forecasttwo">${Math.round(
+              day.temperature.minimum
+            )}°</strong>
+          </div> 
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.maximum
+          )}°</div>
         </div>
       </div>
     `;
   });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
-displayForecast();
+
 // Add submit event listener to the form
 form.addEventListener("submit", submitButton);
 
